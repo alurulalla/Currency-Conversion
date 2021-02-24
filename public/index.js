0b87329a8c56a09f53aaadd407f8276b69ac1fde
Aluru Murali Krishna <alurumuralikrishna9@gmail.com>
2021-02-24 21:19:48 +0530

    Updated to realtime currency

--- public/index.js
+++ public/index.js
@@ -1,20 +1,35 @@
 const inrInputElement = document.getElementById('inr');
 const usdInputElement = document.getElementById('usd');
+const currentRateElement = document.getElementById('current-rate');
 
-inrInputElement.addEventListener('keyup', (e) => {
-  usdInputElement.value = ((+e.target.value * 10) / 72.52).toFixed(2);
+inrInputElement.addEventListener('keyup', async (e) => {
+  const rate = await getLatestConversionRate();
+  usdInputElement.value = ((+e.target.value * 10) / rate).toFixed(2);
 });
 
-inrInputElement.addEventListener('focus', (e) => {
-  usdInputElement.value = ((+e.target.value * 10) / 72.52).toFixed(2);
+inrInputElement.addEventListener('focus', async (e) => {
+  const rate = await getLatestConversionRate();
+  usdInputElement.value = ((+e.target.value * 10) / rate).toFixed(2);
 });
 
-usdInputElement.addEventListener('keyup', (e) => {
-  inrInputElement.value = (+(e.target.value * 0.1 * 72.52)).toFixed(2);
+usdInputElement.addEventListener('keyup', async (e) => {
+  const rate = await getLatestConversionRate();
+  inrInputElement.value = (+(e.target.value * 0.1 * rate)).toFixed(2);
 });
 
-usdInputElement.addEventListener('focus', (e) => {
-  console.log(e.target.value);
+usdInputElement.addEventListener('focus', async (e) => {
+  const rate = await getLatestConversionRate();
   if (e.target.value)
-    inrInputElement.value = (+(e.target.value * 0.1 * 72.52)).toFixed(2);
+    inrInputElement.value = (+(e.target.value * 0.1 * rate)).toFixed(2);
 });
+
+const getLatestConversionRate = async () => {
+  const data = await fetch(
+    'https://api.exchangeratesapi.io/latest?base=USD&symbols=INR'
+  );
+  const rate = await data.json();
+  currentRateElement.innerText = `$${rate.rates.INR.toFixed(2)}`;
+  return rate.rates.INR.toFixed(2);
+};
+
+getLatestConversionRate();
